d561d3ebb6e8c7d32ba791e541897fe3583c0f93
kvxx99 <kvxxccs@gmail.com>
2025-01-24 06:17:29 -0300

    Delete dataset/__init__.py

--- dataset/__init__.py
+++
@@ -1,4 +0,0 @@
-from ultralytics import YOLO
-model = YOLO('yolov8n.pt')  # carrega um modelo YOLO pré-treinado
-model.train(data='/content/drive/MyDrive/YOLO/dataset/data.yaml', epochs=80)  # seu dataset no Drive
-git remote add origin https://github.com/kvxx99/yolo77.git
